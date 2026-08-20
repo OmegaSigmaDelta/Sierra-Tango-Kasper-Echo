14f2f802e0eb3cfa88109ead683290ffa30f9bd9
OmegaSigmaDelta <2010zheka1@gmail.com>
2026-08-20 17:48:08 +0300

    Auto release 20.08.2026-17:47

--- scripts/multiplayer_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_host_button_pressed():
-	MultiplayerManager.host_game()
-
-
-func _on_join_button_pressed():
-	MultiplayerManager.join_game($Control/VBoxContainer/IPAdress.text)
