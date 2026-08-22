f78b0862dbfb635cdb3ed260f355a813c0dde356
OmegaSigmaDelta <2010zheka1@gmail.com>
2026-08-22 16:36:19 +0300

    Auto release 22.08.2026-16:36

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,6 @@
 extends "res://addons/MetroidvaniaSystem/Template/Scripts/MetSysGame.gd"
 
+
 @export_file("room_link") var starting_map: String
 
 
@@ -18,8 +19,23 @@ func _ready() -> void:
 
 func init_room() -> void:
 	var camera := $Ram/Camera2D
+	var room := MetSys.get_current_room_instance()
 
-	camera.global_position = $Ram.global_position
+	room.adjust_camera_limits(camera)
+
+	print("ROOM SIZE: ", room.get_size())
+	print(
+		"CAMERA LIMITS: ",
+		camera.limit_left,
+		", ",
+		camera.limit_top,
+		", ",
+		camera.limit_right,
+		", ",
+		camera.limit_bottom
+	)
 
 	if MetSys.last_player_position.x == Vector2i.MAX.x:
 		MetSys.set_player_position($Ram.position)
+
+	camera.global_position = $Ram.global_position
