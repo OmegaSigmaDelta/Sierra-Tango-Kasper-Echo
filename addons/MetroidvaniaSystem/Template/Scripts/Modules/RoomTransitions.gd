2b98dac863976beb90f892a0b2094228dc04594b
OmegaSigmaDelta <2010zheka1@gmail.com>
2026-08-22 19:44:24 +0300

    Auto release 22.08.2026-19:44

--- addons/MetroidvaniaSystem/Template/Scripts/Modules/RoomTransitions.gd
+++ addons/MetroidvaniaSystem/Template/Scripts/Modules/RoomTransitions.gd
@@ -1,8 +1,7 @@
-extends RefCounted
 extends "res://addons/MetroidvaniaSystem/Template/Scripts/MetSysModule.gd"
 
 
-func _ready() -> void:
+func _initialize() -> void:
 	MetSys.room_changed.connect(_on_room_changed, CONNECT_DEFERRED)
 
 
@@ -10,26 +9,27 @@ func _on_room_changed(target_room: String) -> void:
 	if target_room == MetSys.get_current_room_id():
 		return
 
-	var fade: ColorRect = game.get_node("CanvasLayer/Fade")
 	var prev_room_instance = MetSys.get_current_room_instance()
 
-	# Fade to black
+	# Get the fade from the game scene.
+	var fade: ColorRect = game.get_node("CanvasLayer/Fade")
+
+	# Fade OUT.
 	var tween: Tween = game.get_tree().create_tween()
 	tween.tween_property(fade, "color:a", 1.0, 0.25)
 	await tween.finished
 
-	# Remove old room
+	# IMPORTANT:
+	# Keep the original MetSys room transition behavior.
 	if prev_room_instance:
 		prev_room_instance.get_parent().remove_child(prev_room_instance)
 
-	# Load new room
 	await game.load_room(target_room)
 
-	# Preserve MetSys automatic positioning
 	if prev_room_instance:
-		player.position -= MetSys.get_current_room_instance().get_room_position_offset(prev_room_instance)
+		game.player.position -= MetSys.get_current_room_instance().get_room_position_offset(prev_room_instance)
 		prev_room_instance.queue_free()
 
-	# Fade in
+	# Fade IN.
 	tween = game.get_tree().create_tween()
 	tween.tween_property(fade, "color:a", 0.0, 0.25)
