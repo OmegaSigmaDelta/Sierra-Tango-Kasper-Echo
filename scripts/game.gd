cb2ed8093f42ff52fac0644b4298968a88a1c264
OmegaSigmaDelta <2010zheka1@gmail.com>
2026-08-22 15:09:14 +0300

    Auto release 22.08.2026-15:09

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,10 +1,25 @@
 extends "res://addons/MetroidvaniaSystem/Template/Scripts/MetSysGame.gd"
 
+@export_file("room_link") var starting_map: String
 
-func _ready():
+
+func _ready() -> void:
+	MetSys.reset_state()
+	MetSys.set_save_data()
+
+	set_player($Ram)
+
+	room_loaded.connect(init_room, CONNECT_DEFERRED)
+
+	load_room(starting_map)
+
 	add_module("RoomTransitions.gd")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func init_room() -> void:
+	var camera := $Ram/Camera2D
+
+	camera.global_position = $Ram.global_position
+
+	if MetSys.last_player_position.x == Vector2i.MAX.x:
+		MetSys.set_player_position($Ram.position)
